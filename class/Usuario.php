a31761d9d7726021e1131768c9b413ed4fc4b3e5
DukeKopas <dukekopas@gmail.com>
2017-12-24 03:02:05 -0200

    Métodos de Lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -56,6 +56,46 @@ class Usuario {
 		}
 	}
 
+	public static function getList(){ // esse objeto pode ser estático pois não usou $this
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+	}
+
+	public static function searchUser($user){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			":SEARCH"=>"%".$user."%" // % significa começe com no mySQL
+
+		));
+	}
+
+	public function login($login, $password){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if (count($results) > 0){
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		} else {
+
+			throw new Exception("Login e/ou senha inválidos");
+			
+		}
+	}
+
 	public function __toString(){
 
 		return json_encode(array(
--- index.php
+++ index.php
@@ -5,14 +5,26 @@ require_once ("config.php");
 /* $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * from tb_usuarios");
-
 echo json_encode($usuarios);*/
 
-$root = new Usuario();
+//Carrega um usuário por seu ID
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-$root->loadById(3);
+//Carrega uma lista de usuários ordenados por nome
+//$lista = Usuario::getList(); // Não precisa ser instânciado pois estamos chamando um método estático.
+//echo json_encode($lista); // Tem que usar o json_encode pois estamos usando apenas um método estático e não todo o objeto.
 
-echo $root;
+//Busca por um determinado usuário
+//$user = Usuario :: searchUser("jo");
+//echo json_encode($user);
+
+//Autentica usuário e senha
+$login = new Usuario();
+$login->login("Joao","qwert");
+
+echo $login;
 
  ?>
 
